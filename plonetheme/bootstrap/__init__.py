516b435f003fe387d2767464e805aadd4b774233
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-11-08 08:38:02 +0100

    polish code

--- plonetheme/bootstrap/__init__.py
+++ plonetheme/bootstrap/__init__.py
@@ -1,4 +1,1 @@
-  # -*- extra stuff goes here -*- 
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
+  #
--- plonetheme/bootstrap/setuphandlers.py
+++ plonetheme/bootstrap/setuphandlers.py
@@ -1,11 +1,23 @@
-def setupVarious(context):
+from Products.CMFCore.ActionInformation import Action
+from Products.CMFCore.utils import getToolByName
 
-    # Ordinarily, GenericSetup handlers check for the existence of XML files.
-    # Here, we are not parsing an XML file, but we use this text file as a
-    # flag to check that we actually meant for this import step to be run.
-    # The file is found in profiles/default.
+def setupVarious(context):
+    """ Only in plonetheme.sunburst profile """
 
     if context.readDataFile('plonetheme.bootstrap_various.txt') is None:
         return
 
-    # Add additional setup code here
+    # Do what plonetheme.sunburst does
+    # Copy the plone_setup action to the user category
+    atool = getToolByName(context.getSite(), 'portal_actions')
+    user_category = atool.user
+    if 'plone_setup' in user_category:
+        return
+
+    state = dict(atool.site_actions.plone_setup.propertyItems())
+    state['visible'] = True
+    new_action = Action('plone_setup', **state)
+    user_category['plone_setup'] = new_action
+
+    position = user_category.getObjectPosition('preferences') + 1
+    user_category.moveObjectToPosition('plone_setup', position)

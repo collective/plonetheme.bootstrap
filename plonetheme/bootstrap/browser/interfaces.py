7c8bf6793a929d9e047aed83ab323166dad34427
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-11-08 15:34:08 +0100

    use a browserlayer based interface

--- plonetheme/bootstrap/browser/interfaces.py
+++ plonetheme/bootstrap/browser/interfaces.py
@@ -1,12 +1,8 @@
 from zope.interface import Interface
-from plone.theme.interfaces import IDefaultPloneLayer
 
 
-class IThemeSpecific(IDefaultPloneLayer):
+class IThemeSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer.
-       If you need to register a viewlet only for the
-       "Bootstrap Theme" theme, this interface must be its layer
-       (in bootstrap/viewlets/configure.zcml).
     """
 
 

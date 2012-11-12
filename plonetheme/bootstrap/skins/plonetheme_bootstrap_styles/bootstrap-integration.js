1ac449a276071aa532bb155372e9ad20d2443647
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-11-12 09:28:13 +0100

    comment the actions of this helper JS

--- plonetheme/bootstrap/skins/plonetheme_bootstrap_styles/bootstrap-integration.js
+++ plonetheme/bootstrap/skins/plonetheme_bootstrap_styles/bootstrap-integration.js
@@ -1,23 +1,31 @@
+/*
+ * Helper JS to change classnames and HTML on the fly
+ * because it would be too hard to do it customizing
+ * each template
+ *
+ */
+
 (function($){
 $(document).ready(function(){
-    $('dl.portalMessage:not(#kssPortalMessage)').each(function(){
+
+    /* Automatically generated portal status messages */
+
+    $('dl.portalMessage #kssPortalMessage').each(function(){
         var message = $(this);
         message.removeClass('portalMessage');
         var replacement = $(
-            '<div data-alert="alert" class="alert-message fade in ' + message[0].className + '">' +
-                '<a class="close" href="#">×</a>' +
-                '<p>' + 
-                    '<span class="label">' + message.find('dt').html() + '</span> ' +
+            '<div data-alert="alert" class="alert ' + message[0].className + '">' +
+                    '<strong>' + message.find('dt').html() + '</strong> ' +
                     message.find('dd').html() +
-                '</p>' + 
             '</div>');
         message.replaceWith(replacement);
     });
 
+    /* Convert input[type=buttons] to button tags */
     var foundPrimary = false;
     $('div.formControls input[type="submit"]').each(function(){
         var input = $(this);
-        var button = $('<button type="submit" class="btn" name="' + input.attr('name') + '">' + 
+        var button = $('<button type="submit" class="btn" name="' + input.attr('name') + '">' +
             input.attr('value') + '</button>');
 
         if(input.hasClass('context') && !foundPrimary){
@@ -27,23 +35,41 @@ $(document).ready(function(){
         input.replaceWith(button);
     });
 
+    /* Add btn class to the rest form buttons */
+    $('input[type="submit"], input[type="button"]').addClass('btn');
+
+    /* Edit form tabs */
+
     $('form ul.formTabs').each(function(){
         var ul = $(this);
-        ul.addClass('tabs');
+        ul.addClass('nav');
+        ul.addClass('nav-tabs')
+        ul.removeClass('formTabs');
+        ul.find('li').removeClass('formTab firstFormTab lastFormTab');
+
         ul.find('li:first').addClass('active');
         ul.find('li').click(function(){
             $(this).parent().find('li').removeClass('active');
-            $(this).addClass('active');  
+            $(this).addClass('active');
+        });
+        ul.find('li a span').each(function(){
+            var span = $(this);
+            var a = span.parent().html(span.html);
         });
     });
 
-    $('table.listing').addClass('zebra-striped');
-    $('input[type="submit"],input[type="button"]').addClass('btn');
+    /* Help text in Archetypes forms */
+    $('.formHelp').addClass('help-block').removeClass('formHelp');
 
+    /* Plone's default class for tables */
+    $('table.listing').addClass('zebra-striped');
+
+
+    /*
     $('ul#navigation li[data-dropdown="dropdown"]').hover(function(){
-        $(this).addClass('open'); 
+        $(this).addClass('open');
     }, function(){
-        $(this).removeClass('open'); 
+        $(this).removeClass('open');
     });
     $('ul#navigation li[data-dropdown="dropdown"] ul').hover(function(){}, function(){
         $(this).parent().removeClass('open');
@@ -51,5 +77,6 @@ $(document).ready(function(){
     $('ul#navigation li[data-dropdown="dropdown"] a').click(function(){
         window.location = $(this).attr('href');
     });
+    */
 });
 })(jQuery);

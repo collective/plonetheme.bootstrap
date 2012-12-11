b8a1d28b4a4369c436f933fbdcb1f783e7b2aef1
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-12-11 09:10:13 +0100

    add uninstall support. Fixes #9

--- plonetheme/bootstrap/Extensions/Install.py
+++ plonetheme/bootstrap/Extensions/Install.py
@@ -3,12 +3,44 @@
 from Products.CMFPlone.utils import getToolByName
 
 
+def isPlone42(portal):
+    migration_tool = getToolByName(portal, 'portal_migration')
+    core_versions = migration_tool.coreVersions()
+    plone = core_versions.get('Plone', '')
+    if plone:
+        versions = plone.split('.')
+        return versions[0] == '4' and versions[1] == '2'
+
+    return False
+
+
+def isPlone41(portal):
+    migration_tool = getToolByName(portal, 'portal_migration')
+    core_versions = migration_tool.coreVersions()
+    plone = core_versions.get('Plone', '')
+    if plone:
+        versions = plone.split('.')
+        return versions[0] == '4' and versions[1] == '1'
+
+    return False
+
+
 def uninstall(portal):
     """ Apply the correct so called uninstall-profile when uninstalling
         according to the installed Plone version
     """
 
-    profile = 'profile-plonetheme.bootstrap:uninstall'
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile(profile)
-    return "Ran all uninstall steps."
+    if isPlone42(portal):
+        profile = 'profile-plonetheme.bootstrap:uninstall'
+    elif isPlone41(portal):
+        profile = 'profile-plonetheme.bootstrap:uninstall-plone41'
+    else:
+        profile = ''
+
+    if profile:
+        setup_tool = getToolByName(portal, 'portal_setup')
+        setup_tool.runAllImportStepsFromProfile(profile)
+        return "Ran all uninstall steps."
+
+    else:
+        return "Nothing to uninstall"

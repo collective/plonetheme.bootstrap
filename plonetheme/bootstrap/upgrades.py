6abfbdb9003ece457607d421b7d5ad2d60d58ce8
Mikel Larreategi <mlarreategi@codesyntax.com>
2014-01-24 16:26:26 +0100

    upgrade step

--- plonetheme/bootstrap/upgrades.py
+++ plonetheme/bootstrap/upgrades.py
@@ -14,3 +14,14 @@ def common(context, logger=None):
     setup.runAllImportStepsFromProfile(PROFILE_ID)
 
     logger.info("Migrated from previous version")
+
+
+def remove_old_2_css(context, logger=None):
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger('plonetheme.bootstrap')
+
+    csstool = getToolByName(context, 'portal_css')
+    csstool.unregisterResource('++resource++collective.js.bootstrap/css/bootstrap-responsive.min.css')
+
+    logger.info("Migrated from previous version")
